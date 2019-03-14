a6abf1c9af10ace839eca9e22af591db5f82182f
jianshipeng <email@example.com>
2019-03-14 23:22:18 +0800

    登录页引入

--- src/main/java/com/haoche/yltms/system/controller/LoginController.java
+++ src/main/java/com/haoche/yltms/system/controller/LoginController.java
@@ -1,11 +1,9 @@
 package com.haoche.yltms.system.controller;
 
-import com.haoche.yltms.system.model.User;
 import com.haoche.yltms.system.service.LoginService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class LoginController {
@@ -13,8 +11,7 @@ public class LoginController {
     private LoginService loginService;
 
     @RequestMapping("/login")
-    @ResponseBody
-    public User getUser(){
-        return loginService.getUserByUsername("qwe");
+    public String login(){
+        return "login";
     }
 }
