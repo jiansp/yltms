cecff8404206b3e9acabe97207a45c6b3a5c8d15
Your Name <email@example.com>
2019-03-15 16:07:35 +0800

    Revert "；"
    This reverts commit aa5ad770fb48a96388ccd757c50bb44ba3a77828.

--- src/main/java/com/haoche/yltms/system/controller/LoginController.java
+++ src/main/java/com/haoche/yltms/system/controller/LoginController.java
@@ -1,29 +1,22 @@
 package com.haoche.yltms.system.controller;
 
-import com.haoche.yltms.system.WebSecurityConfig;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.SessionAttribute;
-
-import javax.servlet.http.HttpSession;
 
 @Controller
 public class LoginController {
 
-    @RequestMapping("/")
-    public String index(@SessionAttribute(WebSecurityConfig.SESSION_KEY)String account, Model model){
-        return "index";
-    }
+
 
     @RequestMapping("/login")
     public String login(){
+        return "login";
+    }
+
+    @RequestMapping("/content")
+    public String content(){
         return "content";
     }
 
-    @RequestMapping("/logout")
-    public String logout(HttpSession session){
-        session.removeAttribute(WebSecurityConfig.SESSION_KEY);
-        return "redirect:/login";
-    }
+
 }
