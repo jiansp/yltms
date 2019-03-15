adb16ed72b7ecb12bdaa1e0d36b746ab7b178c2d
Your Name <email@example.com>
2019-03-15 12:30:43 +0800

    不重要的版本测试

--- src/main/java/com/haoche/yltms/system/controller/LoginController.java
+++ src/main/java/com/haoche/yltms/system/controller/LoginController.java
@@ -1,17 +1,29 @@
 package com.haoche.yltms.system.controller;
 
-import com.haoche.yltms.system.service.LoginService;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.haoche.yltms.system.WebSecurityConfig;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttribute;
+
+import javax.servlet.http.HttpSession;
 
 @Controller
 public class LoginController {
-    @Autowired
-    private LoginService loginService;
+
+    @RequestMapping("/")
+    public String index(@SessionAttribute(WebSecurityConfig.SESSION_KEY)String account, Model model){
+        return "index";
+    }
 
     @RequestMapping("/login")
     public String login(){
-        return "login";
+        return "content";
+    }
+
+    @RequestMapping("/logout")
+    public String logout(HttpSession session){
+        session.removeAttribute(WebSecurityConfig.SESSION_KEY);
+        return "redirect:/login";
     }
 }
