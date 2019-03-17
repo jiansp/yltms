accc7e9e52b6a55ff1fd29adebb5611490c2628e
jianshipeng <email@example.com>
2019-03-17 23:08:03 +0800

    登录和框架

--- src/main/java/com/haoche/yltms/config/LoginInterceptor.java
+++ src/main/java/com/haoche/yltms/config/LoginInterceptor.java
@@ -19,12 +19,12 @@ public class LoginInterceptor implements HandlerInterceptor {
         //每一个项目对于登陆的实现逻辑都有所区别，我这里使用最简单的Session提取User来验证登陆。
         HttpSession session = request.getSession();
         //这里的User是登陆时放入session的
-        User user = (User) session.getAttribute("user");
+        User user = (User) session.getAttribute("USER");
         //如果session中没有user，表示没登陆
         if (user == null){
             //这个方法返回false表示忽略当前请求，如果一个用户调用了需要登陆才能使用的接口，如果他没有登陆这里会直接忽略掉
             //当然你可以利用response给用户返回一些提示信息，告诉他没登陆
-            String url = "/login";
+            String url = "/";
             try {
                 response.sendRedirect(url);
             } catch (IOException e) {
--- src/main/java/com/haoche/yltms/system/controller/LoginController.java
+++ src/main/java/com/haoche/yltms/system/controller/LoginController.java
@@ -1,21 +1,43 @@
 package com.haoche.yltms.system.controller;
 
+import com.haoche.yltms.system.model.User;
+import com.haoche.yltms.system.service.LoginService;
+import com.haoche.yltms.system.vo.Result;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpSession;
 
 @Controller
 public class LoginController {
+    @Autowired
+    private LoginService loginService;
 
 
-
-    @RequestMapping("/login")
-    public String login(){
+    @RequestMapping("/")
+    public String login() {
         return "login";
     }
 
-    @RequestMapping("/content")
-    public String content(){
-        return "content";
+    @RequestMapping("/login")
+    @ResponseBody
+    public Result content(HttpSession session, String username, String password) {
+        User user = this.loginService.getUserByUsername(username);
+        if(user != null){
+            session.setAttribute("USER", user);
+        }
+        Result result = new Result();
+        result.setSuccess(true);
+        result.setMsg(username + password);
+        result.setObj(session.getAttribute("USER"));
+        return result;
+    }
+
+    @RequestMapping("/index")
+    public String index() {
+        return "index";
     }
 
 
