d0a1589815ae992654c8e2c760c8a51110ffcc70
jianshipeng <email@example.com>
2019-04-03 22:52:25 +0800

    passedit

--- src/main/java/com/haoche/yltms/system/controller/RouteController.java
+++ src/main/java/com/haoche/yltms/system/controller/RouteController.java
@@ -4,8 +4,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/order")
-public class OrderController {
+@RequestMapping("/route")
+public class RouteController {
 
     @RequestMapping("/browseVehicle")
     public String browseVehicle(){
