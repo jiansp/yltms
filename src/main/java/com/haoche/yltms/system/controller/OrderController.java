8d9126b5ff85e2b40c89d8dbfbacbab52ba83f31
Your Name <email@example.com>
2019-04-03 20:23:59 +0800

    11

---
+++ src/main/java/com/haoche/yltms/system/controller/OrderController.java
@@ -0,0 +1,14 @@
+package com.haoche.yltms.system.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/order")
+public class OrderController {
+
+    @RequestMapping("/browseVehicle")
+    public String browseVehicle(){
+        return "booking";
+    }
+}
