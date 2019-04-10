69a41daaddd92f8ece510dd3811190e19454174a
jianshipeng <email@example.com>
2019-04-10 23:27:46 +0800

    拯救者

--- src/main/resources/static/js/encode64.js
+++ src/main/resources/static/js/encode64.js
@@ -27,3 +27,20 @@ function encode64(input) {
     return output;
 }
 // base64加密结束
+
+$.fn.serializeObject = function()
+{
+    var o = {};
+    var a = this.serializeArray();
+    $.each(a, function() {
+        if (o[this.name]) {
+            if (!o[this.name].push) {
+                o[this.name] = [o[this.name]];
+            }
+            o[this.name].push(this.value || '');
+        } else {
+            o[this.name] = this.value || '';
+        }
+    });
+    return o;
+};
